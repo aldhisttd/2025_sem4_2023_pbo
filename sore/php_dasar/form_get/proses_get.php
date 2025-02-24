2a9f242e8cb4a0001ca84a6404a23b79ee6061ef
Aldhi <aldhi.albadri@gmail.com>
2025-02-24 20:40:05 +0700

    add form get submit

---
+++ sore/php_dasar/form_get/proses_get.php
@@ -0,0 +1,6 @@
+<?php 
+
+$user = $_GET['username'];
+$pass = $_GET['password'];
+
+echo "Username nya ".$user." dan passwordnya ".$pass;
---
+++ sore/php_dasar/varible_url.php
@@ -0,0 +1,5 @@
+<?php 
+
+echo $_GET['nama'];
+
+// http://localhost:800/www/kampus/2025_sem4_2023_pbo/sore/php_dasar/varible_url.php?nama=budi
