5fb58ddb30d15385c949dc18354c2864e326b46f
Aldhi <aldhi.albadri@gmail.com>
2025-03-17 12:26:41 +0700

    add logindb

---
+++ pagib/php_dasar/logindb/proses/proses_login.php
@@ -0,0 +1,44 @@
+<?php 
+if(!isset($_POST['submit'])){
+    header('location:../form_login.php');
+}
+
+$userForm = $_POST['username'];
+$passForm = $_POST['password'];
+session_start();
+
+// cek data input kosong atau
+if($userForm==''){
+    $_SESSION['err']['username'] = "Username tidak boleh kosong";
+}
+if($passForm==''){
+    $_SESSION['err']['password'] = "Password tidak boleh kosong";
+}
+if($userForm=='' || $passForm==''){
+    header('location:../form_login.php');
+    exit();
+}
+
+// cek data login
+// if($userForm!=$userTrue || $passForm!=$passTrue){
+//     $_SESSION['err']['login_fail'] = "Data login tidak valid.";
+//     header('location:../form_login.php');
+//     exit();
+// }
+
+// koneksi
+$koneksi = mysqli_connect('localhost', 'root', '', 'pbo_pagib');
+$sql = "SELECT * FROM `user` WHERE username='$userForm' AND password='$passForm'";
+$hasil = mysqli_query($koneksi, $sql);
+$row = mysqli_num_rows($hasil);
+
+if($row==0){
+    $_SESSION['err']['login_fail'] = "Data login tidak valid.";
+    header('location:../form_login.php');
+    exit();
+}
+
+//login success
+$_SESSION['login'] = true;
+header('location:../dashboard.php');
+exit();
