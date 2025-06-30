1fe80e26deaf2214c1d1f961e1880eaa3b409db2
Aldhi <aldhi.albadri@gmail.com>
2025-06-30 19:07:29 +0700

    fix templating project

---
+++ sore/swalayan/konten/pelanggan.php
@@ -0,0 +1,21 @@
+<div class="row">
+    <div class="col-12">
+        <div class="page-title-box d-flex align-items-center justify-content-between">
+            <h4 class="mb-0">Pelanggan</h4>
+
+            <div class="page-title-right">
+                <ol class="breadcrumb m-0">
+                    <li class="breadcrumb-item"><a href="javascript: void(0);">Utility</a></li>
+                    <li class="breadcrumb-item active">Pelanggan</li>
+                </ol>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti voluptatum commodi cumque in, pariatur quae incidunt dolor mollitia, asperiores recusandae laudantium aspernatur labore magni! Ea facere beatae ab velit qui.
+    </div>
+</div>
